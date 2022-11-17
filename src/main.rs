90c9af1a86bac1e240cdef664bbde40d7ba729e2
VladStriletchi <vladstriletchi@gmail.com>
2022-11-17 20:15:20 +0200

    done prototype

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,48 @@
+use bevy::ecs::query;
+use rand::prelude::SliceRandom;
+
 use bevy::prelude::*;
+use bevy::time::FixedTimestep;
+use bevy::app::AppExit;
+use rand::prelude::random;
+use bevy::ecs::system::Query;
 
 const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
+const SNAKE_SEGMENT_COLOR: Color = Color::rgb(0.3, 0.3, 0.3);
+const FOOD_COLOR: Color = Color::rgb(1.0, 0.0, 1.0);
 const ARENA_WIDTH: u32 = 10;
 const ARENA_HEIGHT: u32 = 10;
 
+struct GrowthEvent;
+struct GameOverEvent;
+
+
 #[derive(Component)]
-struct SnakeHead;
+struct SnakeHead{
+    direction: Direction,
+}
+
+#[derive(Component)]
+struct SnakeSegment;
+
+#[derive(Default, Deref, DerefMut, Resource)]
+struct SnakeSegments(Vec<Entity>);
+
+
+#[derive(Component)]
+struct Food;
 
 #[derive(Component, Clone, Copy, PartialEq, Eq)]
 struct Position {
     x: i32,
     y: i32,
 }
+
+#[derive(Default, Resource)]
+struct LastTailPosition(Option<Position>);
+
+#[derive(Component, Default, Resource)]
+struct FreePosition(Option<Position>);
 
 #[derive(Component)]
 struct Size {
@@ -23,6 +54,25 @@ impl Size {
         Self {
             width: x,
             height: x,
+        }
+    }
+}
+
+#[derive(PartialEq, Copy, Clone)]
+enum Direction {
+    Left,
+    Up,
+    Right,
+    Down,
+}
+
+impl Direction {
+    fn opposite(self) -> Self {
+        match self {
+            Self::Left => Self::Right,
+            Self::Right => Self::Left,
+            Self::Up => Self::Down,
+            Self::Down => Self::Up,
         }
     }
 }
@@ -57,56 +107,246 @@ fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
-fn spawn_snake(mut commands: Commands) {
+fn spawn_snake(mut commands: Commands, mut segments: ResMut<SnakeSegments>) {
+    *segments = SnakeSegments(vec![
+        commands
+            .spawn(SpriteBundle {
+                sprite: Sprite {
+                    color: SNAKE_HEAD_COLOR,
+                    ..default()
+                },
+                ..default()
+            })
+            .insert(SnakeHead {
+                direction: Direction::Up,
+            })
+            .insert(SnakeSegment)
+            .insert(Position { x: 3, y: 3 })
+            .insert(Size::square(1.0))
+            .id(),
+        spawn_segment(commands, Position { x: 3, y: 2 }),
+        
+    ]);
+}
+
+fn spawn_segment(mut commands: Commands, position: Position) -> Entity {
     commands
         .spawn(SpriteBundle {
             sprite: Sprite {
-                color: SNAKE_HEAD_COLOR,
-                ..default()
-            },
-            transform: Transform {
-                scale: Vec3::new(10.0, 10.0, 10.0),
+                color: SNAKE_SEGMENT_COLOR,
                 ..default()
             },
             ..default()
         })
-        .insert(SnakeHead)
-        .insert(Position { x: 3, y: 3 }) // <--
+        .insert(SnakeSegment)
+        .insert(position)
+        .insert(Size::square(0.65))
+        .id()
+}
+fn spawn_food(mut commands: Commands){
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: FOOD_COLOR,
+                ..default()
+            },
+            ..default()
+        })
+        .insert(Food)
+        .insert(Position {
+            x: (random::<f32>() * ARENA_WIDTH as f32) as i32,
+            y: (random::<f32>() * ARENA_HEIGHT as f32) as i32,
+        })
         .insert(Size::square(0.8));
+        
 }
 
-fn snake_movement(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut head_positions: Query<&mut Position, With<SnakeHead>>,
-) {
-    for mut pos in head_positions.iter_mut() {
-        if keyboard_input.pressed(KeyCode::Left) {
-            pos.x -= 1;
-        }
-        if keyboard_input.pressed(KeyCode::Right) {
-            pos.x += 1;
-        }
-        if keyboard_input.pressed(KeyCode::Down) {
-            pos.y -= 1;
-        }
-        if keyboard_input.pressed(KeyCode::Up) {
-            pos.y += 1;
+fn snake_movement_input(keyboard_input: Res<Input<KeyCode>>, mut heads: Query<&mut SnakeHead>) {
+    if let Some(mut head) = heads.iter_mut().next() {
+        let dir: Direction = if keyboard_input.pressed(KeyCode::Left) {
+            Direction::Left
+        } else if keyboard_input.pressed(KeyCode::Down) {
+            Direction::Down
+        } else if keyboard_input.pressed(KeyCode::Up) {
+            Direction::Up
+        } else if keyboard_input.pressed(KeyCode::Right) {
+            Direction::Right
+        } else {
+            head.direction
+        };
+        if dir != head.direction.opposite() {
+            head.direction = dir;
         }
     }
 }
 
+fn snake_movement(
+    segments: ResMut<SnakeSegments>,
+    mut heads: Query<(Entity, &SnakeHead)>,
+    mut positions: Query<&mut Position>,
+    mut last_tail_position: ResMut<LastTailPosition>,
+    mut game_over_writer: EventWriter<GameOverEvent>,
+) {
+    if let Some((head_entity, head)) = heads.iter_mut().next() {
+        let segment_positions = segments
+            .iter()
+            .map(|e| *positions.get_mut(*e).unwrap())
+            .collect::<Vec<Position>>();
+        let mut head_pos = positions.get_mut(head_entity).unwrap();
+        match &head.direction {
+            Direction::Left => {
+                head_pos.x -= 1;
+            }
+            Direction::Right => {
+                head_pos.x += 1;
+            }
+            Direction::Up => {
+                head_pos.y += 1;
+            }
+            Direction::Down => {
+                head_pos.y -= 1;
+            }
+        };
+        if head_pos.x < 0
+        || head_pos.y < 0
+        || head_pos.x as u32 >= ARENA_WIDTH
+        || head_pos.y as u32 >= ARENA_HEIGHT
+        {
+            game_over_writer.send(GameOverEvent);
+        }
+        if segment_positions.contains(&head_pos) {
+            game_over_writer.send(GameOverEvent);
+        }
+        segment_positions
+            .iter()
+            .zip(segments.iter().skip(1))
+            .for_each(|(pos, segment)| {
+                *positions.get_mut(*segment).unwrap() = *pos;
+            });
+        
+        *last_tail_position = LastTailPosition(Some(*segment_positions.last().unwrap()));
+    }
+}
+
+fn snake_eating(
+    mut commands: Commands,
+    mut growth_writer: EventWriter<GrowthEvent>,
+    food_positions: Query<(Entity, &Position), With<Food>>,
+    head_positions: Query<&Position, With<SnakeHead>>,
+) {
+    for head_pos in head_positions.iter() {
+        for (ent, food_pos) in food_positions.iter() {
+            if food_pos == head_pos {
+                commands.entity(ent).despawn();
+                growth_writer.send(GrowthEvent);
+            }
+        }
+    }
+}
+
+fn snake_growth(
+    commands: Commands,
+    last_tail_position: Res<LastTailPosition>,
+    mut segments: ResMut<SnakeSegments>,
+    mut growth_reader: EventReader<GrowthEvent>,
+) {
+    if growth_reader.iter().next().is_some() {
+        segments.push(spawn_segment(commands, last_tail_position.0.unwrap()));
+    }
+}
+
+fn empty_position(
+    occupied_positions: Query<&Position, With<SnakeSegment>>,
+    mut free_position: ResMut<FreePosition>,
+){
+    let mut all_positions = Vec::new();
+    for i in 0..ARENA_HEIGHT{
+        for j in 0..ARENA_WIDTH{
+            all_positions.push(Position{y:i as i32, x:j as i32})
+        }
+    }
+
+    let mut empty_positions = all_positions;
+    for pos in occupied_positions.iter(){
+        empty_positions.retain(|x| x != pos);
+    }
+    
+    let result = empty_positions.choose(&mut rand::thread_rng()).unwrap();
+    free_position.0 = Some(*result);
+}
+
+fn food_spawner(
+    mut commands: Commands,
+    free_position: Res<FreePosition>,
+    mut growth_event_listener: EventReader<GrowthEvent>,
+) {
+    if growth_event_listener.iter().next().is_some(){
+        commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: FOOD_COLOR,
+                ..default()
+            },
+            ..default()
+        })
+        .insert(Food)
+        // .insert(Position {
+        //     x: (random::<f32>() * ARENA_WIDTH as f32) as i32,
+        //     y: (random::<f32>() * ARENA_HEIGHT as f32) as i32,
+        // })
+        .insert(free_position.0.unwrap())
+        .insert(Size::square(0.8));
+    }   
+}
+
+fn exit_system(mut exit: EventWriter<AppExit>, mut gameover: EventReader<GameOverEvent>) {
+    if gameover.iter().next().is_some(){
+        exit.send(AppExit);
+    }
+    
+}
+
 fn main() {
     App::new()
+    .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+    .insert_resource(SnakeSegments::default())
+    .insert_resource(LastTailPosition::default())
+    .insert_resource(FreePosition::default())
     .add_startup_system(setup_camera)
     .add_startup_system(spawn_snake)
-    .add_system(snake_movement)
+    .add_startup_system(spawn_food)
+    .add_system(exit_system)
+    .add_system(snake_movement_input.before(snake_movement))
+    .add_system_set(
+        SystemSet::new()
+            .with_run_criteria(FixedTimestep::step(0.150))
+            .with_system(snake_movement)
+            .with_system(snake_eating.after(snake_movement))
+            .with_system(snake_growth.after(snake_eating)),
+    )
     .add_system_set_to_stage(
         CoreStage::PostUpdate,
         SystemSet::new()
             .with_system(position_translation)
             .with_system(size_scaling),
     )
-    .add_plugins(DefaultPlugins)
+    .add_system_set(
+        SystemSet::new()
+            //.with_run_criteria(FixedTimestep::step(1.0))
+            .with_system(empty_position)
+            .with_system(food_spawner.after(empty_position)),
+    )
+    .add_event::<GrowthEvent>()
+    .add_event::<GameOverEvent>()
+    .add_plugins(DefaultPlugins.set(WindowPlugin {
+        window: WindowDescriptor {
+            title: "Carriage Snake!".to_string(), 
+            width: 500.0,                 
+            height: 500.0,
+          ..default()
+        },
+        ..default()
+      }))
     .run();
 
 }
